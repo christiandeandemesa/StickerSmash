d298cae5bca7e09db8237526233bf7c5f87f80d8
Christian Dean <christiandeandemesa@gmail.com>
2023-01-27 10:13:53 -0800

    initial design

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+import PlaceholderImage from "./assets/images/background-image.png";
+
+function App() {
   return (
+    // All core components in React Native have a style prop.
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// The style prop used above accepts a JS object as its value, so styling is done here instead of CSS.
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  footContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
+
+export default App;
+
---
+++ components/Button.js
@@ -0,0 +1,74 @@
+import { StyleSheet, View, Pressable, Text } from "react-native";
+
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+function Button({ label, theme }) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          // Takes the default style from buttonContainer, then adds or overrides with the inline styles.
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={() => alert("You pressed a button.")}
+        >
+          {/* Image icon */}
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
+
+  return (
+    <View style={styles.buttonContainer}>
+      {/* The Pressable component that detects various types of interaction (e.g. single-tap or long press events). */}
+      <Pressable
+        style={styles.button}
+        // Creates a typical phone alert.
+        onPress={() => alert("You pressed a button")}
+      >
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 3,
+  },
+  button: {
+    borderRadius: 10,
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
+
+export default Button;
